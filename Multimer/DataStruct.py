c44c0c7a02e664614b36a5014e26e23c2355b561
KaptajnBimse <jmh@calaha.dk>
2024-04-08 15:02:26 +0200

    Alignment virker for monomer

--- Multimer/DataStruct.py
+++ Multimer/DataStruct.py
@@ -1,14 +1,77 @@
 from Bio.PDB import PDBParser
+import Bio.PDB
+from Bio.SeqUtils import IUPACData
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from Bio.PDB.Polypeptide import PPBuilder
+from Bio import Align
+from PDBP_to_seq import two_PDBP_to_seq
 
-p = PDBParser(QUIET=True)
-s = p.get_structure("s", "examples/Multimer PDB/1y8k.pdb")
+P1, P2, seq1, seq2, ref_structure, sample_structure= two_PDBP_to_seq("C:/Users/Kapta/Documents/Skole/DTU/6.semester/BP/Detection-of-topological-changes-in-multimer-protein-structures/Multimer/examples/T1026.pdb", "C:/Users/Kapta/Documents/Skole/DTU/6.semester/BP/Detection-of-topological-changes-in-multimer-protein-structures/Multimer/examples/T1026TS015_5")
 
-ca = [(atom.full_id[2], atom.full_id[3][1], *atom.get_coord()) for atom in s.get_atoms() if atom.full_id[4][0] == "CA"]
+
+aligner = Align.PairwiseAligner()
+
+alignments = aligner.align(seq1["Chain_ "], seq2["Chain_ "])
+# print(len(alignments))
+# for alignment in sorted(alignments):
+
+#     print("Score = %.1f:" % alignment.score)
+
+#     print(alignment)
+
+align = alignments[0]
+
+# Select what residues numbers you wish to align
+# and put them in a list
+start_id = align.aligned[1][0][0]+1
+end_id   = align.aligned[1][0][1]
+atoms_to_be_aligned = range(start_id, end_id + 1)
+
+# Use the first model in the pdb-files for alignment
+# Change the number 0 if you want to align to another structure
+ref_model    = ref_structure[0]
+sample_model = sample_structure[0]
+
+# Make a list of the atoms (in the structures) you wish to align.
+# In this case we use CA atoms whose index is in the specified range
+ref_atoms = []
+sample_atoms = []
+
+# Iterate of all chains in the model in order to find all residues
+for ref_chain in ref_model:
+  # Iterate of all residues in each model in order to find proper atoms
+  for ref_res in ref_chain:
+    # Check if residue number ( .get_id() ) is in the list
+    if ref_res.get_id()[1] in atoms_to_be_aligned:
+      # Append CA atom to list
+      ref_atoms.append(ref_res['CA'])
+
+
+# Do the same for the sample structure
+for sample_chain in sample_model:
+  for sample_res in sample_chain:
+    if sample_res.get_id()[1] in atoms_to_be_aligned:
+      sample_atoms.append(sample_res['CA'])
+
+# Now we initiate the superimposer:
+super_imposer = Bio.PDB.Superimposer()
+super_imposer.set_atoms(ref_atoms, sample_atoms)
+super_imposer.apply(sample_model.get_atoms())
+
+# Print RMSD:
+print(super_imposer.rms)
+
+# Save the aligned version of 1UBQ.pdb
+io = Bio.PDB.PDBIO()
+io.set_structure(sample_structure) 
+aligned = io.structure
+
+
+ca = [(atom.full_id[2], atom.full_id[3][1], *atom.get_coord()) for atom in aligned.get_atoms() if atom.full_id[4][0] == "CA"]
 df = pd.DataFrame(ca, columns=["chain", "residue_number", "x", "y", "z"])
-#df
+
 
 df["P"] = df[["x", "y", "z"]].values.tolist()
 
@@ -17,21 +80,17 @@ P = {}
 for chain in df["chain"].unique():
     P["Chain_" + chain] = df[df["chain"] == chain]["P"]
 
+#Plot P1, P2 and P in 3d using plotly
+import plotly.graph_objects as go
+fig = go.Figure()
 
-from Bio.pairwise2 import nwalign
+for chain in P1.keys():
+    fig.add_trace(go.Scatter3d(x=[i[0] for i in P1[chain]], y=[i[1] for i in P1[chain]], z=[i[2] for i in P1[chain]], mode='lines', line=dict(width=9), name=chain))
 
-# Define two sequences
-seq1 = "ACCGT"
-seq2 = "ACG"
+# for chain in P2.keys():
+#     fig.add_trace(go.Scatter3d(x=[i[0] for i in P2[chain]], y=[i[1] for i in P2[chain]], z=[i[2] for i in P2[chain]], mode='lines', line=dict(width=9), name=chain))
 
-# Perform Needleman-Wunsch alignment
-alignments = nwalign(seq1, seq2)
+for chain in P.keys():
+    fig.add_trace(go.Scatter3d(x=[i[0] for i in P[chain]], y=[i[1] for i in P[chain]], z=[i[2] for i in P[chain]], mode='lines', line=dict(width=9), name=chain))
 
-# Print the alignment results
-for alignment in alignments:
-    print("Alignment Score:", alignment.score)
-    print("Sequence 1:", alignment.seqA)
-    print("Sequence 2:", alignment.seqB)
-    print("Alignment Start:", alignment.start)
-    print("Alignment End:", alignment.end)
-    print()
+fig.show()
---
+++ Multimer/PDBP_to_seq.py
@@ -0,0 +1,70 @@
+from Bio.PDB import PDBParser
+import pandas as pd
+from Bio.PDB.Polypeptide import PPBuilder
+
+def one_PDB_to_seq(PDB_filename):
+
+    p = PDBParser(QUIET=True)
+    s1 = p.get_structure("s1", PDB_filename)
+
+    ca1 = [(atom.full_id[2], atom.full_id[3][1], *atom.get_coord()) for atom in s1.get_atoms() if atom.full_id[4][0] == "CA"]
+    df1 = pd.DataFrame(ca1, columns=["chain", "residue_number", "x", "y", "z"])
+    
+
+    df1["P"] = df1[["x", "y", "z"]].values.tolist()
+
+    P1 = {}
+    
+    for chain in df1["chain"].unique():
+        P1["Chain_" + chain] = df1[df1["chain"] == chain]["P"]
+
+    # Get the sequence
+    seq1 = {}
+    ppb=PPBuilder()
+    i = 0
+    for pp in ppb.build_peptides(s1):
+        seq1["Chain_" + df1["chain"].unique()[i]] = pp.get_sequence()
+        i += 1
+    i = 0
+    return P1, seq1
+
+
+def two_PDBP_to_seq(PDB1_filename, PDB2_filename):
+
+    p = PDBParser(QUIET=True)
+    s1 = p.get_structure("s1", PDB1_filename)
+    s2 = p.get_structure("s2", PDB2_filename)
+
+    ca1 = [(atom.full_id[2], atom.full_id[3][1], *atom.get_coord()) for atom in s1.get_atoms() if atom.full_id[4][0] == "CA"]
+    df1 = pd.DataFrame(ca1, columns=["chain", "residue_number", "x", "y", "z"])
+    
+    ca2 = [(atom.full_id[2], atom.full_id[3][1], *atom.get_coord()) for atom in s2.get_atoms() if atom.full_id[4][0] == "CA"]
+    df2 = pd.DataFrame(ca2, columns=["chain", "residue_number", "x", "y", "z"])
+
+    df1["P"] = df1[["x", "y", "z"]].values.tolist()
+    df2["P"] = df2[["x", "y", "z"]].values.tolist()
+
+    P1 = {}
+    P2 = {}
+    if len(df1["chain"].unique()) != len(df2["chain"].unique()):
+        print("The number of chains is different between the two structures")
+        return
+    
+    for chain in df1["chain"].unique():
+        P1["Chain_" + chain] = df1[df1["chain"] == chain]["P"]
+        P2["Chain_" + chain] = df2[df2["chain"] == chain]["P"]
+
+    # Get the sequence
+    seq1 = {}
+    seq2 = {}
+    ppb=PPBuilder()
+    i = 0
+    for pp in ppb.build_peptides(s1):
+        seq1["Chain_" + df1["chain"].unique()[i]] = pp.get_sequence()
+        i += 1
+    i = 0
+    for pp in ppb.build_peptides(s2):
+        seq2["Chain_" + df2["chain"].unique()[i]] = pp.get_sequence()
+        i += 1
+
+    return P1, P2, seq1, seq2, s1, s2
